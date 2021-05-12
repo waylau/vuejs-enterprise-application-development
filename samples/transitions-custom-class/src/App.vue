253f81caf182ccb4a331c1aec01b95aceedcb8cf
waylau <waylau521@gmail.com>
2021-05-13 00:35:30 +0800

    add demo: * [自定义过渡class类名的例子](samples/transitions-custom-class)

---
+++ samples/transitions-custom-class/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<link
+  href="https://cdn.bootcdn.net/ajax/libs/animate.css/4.1.1/animate.min.css"
+  rel="stylesheet"
+  type="text/css"
+/>
+<div>
+  <button @click="show = !show">
+    点击切换
+  </button>
+
+  <transition
+    name="custom-classes-transition"
+    enter-active-class="animate__animated animate__tada"
+    leave-active-class="animate__animated animate__bounceOutRight"
+  >
+    <p v-if="show">我显示出来了</p>
+  </transition>
+</div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class App extends Vue {
+  private show: boolean = true;
+}
+</script>
